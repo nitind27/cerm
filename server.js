6d4ac06e4cbf6a66148293ccdcbd0671e32dfcef
Nitin Dube <nitindube227@gmail.com>
2025-02-21 13:57:11 +0530

    edd export

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// // server.js
-// const express = require("express");
-// const cors = require("cors");
-
-// // Routes
-// const propertyRoutes = require("./routes/propertyRoutes");
-// const renterRoutes = require("./routes/renterRoutes");
-
-// const app = express();
-// const PORT = 3001;
-
-// // Middleware
-// app.use(cors());                               // Allow cross-origin requests
-// app.use(express.json());                       // Parse JSON request bodies
-// app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
-// app.use("/uploads", express.static("uploads"));  // Serve files from /uploads if needed
-
-// // Routes
-// // - Property endpoints
-// app.use("/api/property", propertyRoutes);
-
-// // - Renter endpoints
-// app.use("/api/renter", renterRoutes);
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-
 
 // server.js
 const express = require("express");
@@ -59,3 +30,5 @@ app.use("/api/renter", renterRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
